eb66f4764f24345b0f82ca1c8ccb83eb7a4abda1
Kay <kthss01@naver.com>
2015-11-12 12:39:09 +0900

    11/12 test2

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -7,5 +7,6 @@ public class AppMain {
     public static void main(String args[]) {
 
         System.out.println("Test Git to Yut Game");
+        System.out.println("Check GitHub upload");
     }
 }
---
+++ src/main/java/Yut.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ±èÅÂÈÆ on 2015-11-12.
+ */
+public class Yut {
+    public Yut() {
+
+    }
+}
